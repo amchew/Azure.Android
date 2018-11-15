105089f7686b8457141e576c5d018d6c69baf2e7
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-11-15 18:19:12 +0300

    Code signing + distribute

--- AzureDataAndroidExample_Java/app/src/main/java/com/azure/mobile/azuredataandroidexample_java/Controllers/App.java
+++ AzureDataAndroidExample_Java/app/src/main/java/com/azure/mobile/azuredataandroidexample_java/Controllers/App.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.azure.data.AzureData;
 import com.azure.data.model.PermissionMode;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.distribute.Distribute;
 
 import static com.azure.data.util.FunctionalUtils.onCallback;
 
@@ -13,6 +15,7 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
+        AppCenter.start(this, "49370b39-3b0d-40d7-92fe-4e7bc3c45b1f", Distribute.class);
         // configure AzureData - fill in your account name and master key
         AzureData.configure(getApplicationContext(), "", "", PermissionMode.All, onCallback(builder -> {
 
